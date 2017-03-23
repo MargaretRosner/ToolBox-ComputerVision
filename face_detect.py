5a8af1b86dfea413733f1e6df1b42f87a754c37b
MargaretRosner <rosner.margaret@gmail.com>
2017-03-23 15:10:48 -0400

    I could not get the video camera to work despite working on the toolbox for three hours and with the help of two NINJA's. It was frustrating.

--- face_detect.py
+++ face_detect.py
@@ -1,4 +1,44 @@
 """ Experiment with face detection and image filtering using OpenCV """
-
-import cv2
+print('foo')
 import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(-1)
+print cap.isOpened()
+while True:
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    print ret
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
+
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
